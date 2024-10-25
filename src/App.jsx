bada07b1c46e150bab7413fe3280d8a9131be7b4
Aakib01 <mohamedaakibali@gmail.com>
2024-10-25 19:02:36 +0530

    Added Player info

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import Player from "./components/Player";
 function App() {
-  
-
   return (
-    <h1>Coming Soon....</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
+        </ol>
+        Game
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
